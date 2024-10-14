4f025c84186ac4545398d7675c42adb44aef0ac8
SungChanYoung <zkzk7290@naver.com>
2024-10-14 21:01:48 +0900

    20241014

---
+++ src/main/java/org/example/springbootdeveloper/MemberRepository.java
@@ -0,0 +1,11 @@
+package org.example.springbootdeveloper;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+//DB에서 데이터를 가져오는 퍼시스턴스계층
+// member 테이블에서 접근해서 member클래스 에 매핑하는 구현체
+public interface MemberRepository  extends JpaRepository<Member,Long> {
+
+}
---
+++ src/main/java/org/example/springbootdeveloper/이론/e_IoC_DI.java
@@ -0,0 +1,28 @@
+package org.example.springbootdeveloper.이론;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class e_IoC_DI {
+    public static void main(String[] args) {
+//        BookStore2 store2 = new BookStore2();
+//        store2.displayBook();
+
+        // 정말로중요한 개념
+        // 1.제어의 역전 (Inversion of Control)
+//      : 프로그램 의 제어 흐름을 개발자가 아닌 '외부 컨테이너'에 위임하는 방식
+//      제어의 권한이 컨테이너 에게 넘어가 갹체의 생명 주기를 컨테이너가 관리
+//    cf) 전통적인 프로그래밍
+//    : 개발자가 객체의 생성부터 소멸 까지 직접 제어
+//
+        // 2. 의존성 주입
+//    DI (Dependency Injection )
+//    : 제어의 역전을 구현하기 위해 사용하는방법 어떤 클래스가 다른 클래스에 의존한다
+//    객체 간의 결합도를 낮추고 유연성과 재사용성을 높이며 코드의 테스트가 용이
+//    생성자 주입 ,필드 주입 세터 주입등의 방식으로 이루어짐
+//    @autowired: 스프링 컨테이너에 있는 빈 이라는것 주입하는 역할
+//        cf) 빈-콩 (Bean): 스프링 컨테이너에서 관리하는 객체
+//
+    }
+
+}
